dd8c037d56158351c3c710816620b217721014bf
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2025-11-09 07:15:28 -0500

    improved front

--- frontend/dourememberfront/src/app/components/patient-edit-profile/patient-edit-profile.ts
+++ frontend/dourememberfront/src/app/components/patient-edit-profile/patient-edit-profile.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PatientsService } from './../../services/patientsService';
 
 @Component({
   selector: 'patient-edit-profile',
   imports: [RouterModule],
   templateUrl: './patient-edit-profile.html',
 })
-export class PatientEditProfile { }
+export class PatientEditProfile {
+
+  userId: number | null = 0;
+
+  constructor(private readonly PatientsService: PatientsService){
+  this.userId = this.PatientsService.getUserId();
+  }
+}
 
